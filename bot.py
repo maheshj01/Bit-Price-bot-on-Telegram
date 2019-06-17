760935308b2dfa70e3e94a9477223869994a0f22
Mahesh <maheshmn121@gmail.com>
2019-06-17 22:24:17 +0530

    updated bot

--- bot.py
+++ bot.py
@@ -2,11 +2,14 @@ import telebot
 import requests
 import time
 import json
+from flask import Flask
+import os
 
-bot_token = '<bot token>'
+
+bot_token = '477501756:AAFW8vtaZyVe3qF9r-YJqWLFax_ru33-XCM'
 api_url = "https://api.telegram.org/bot{}/".format(bot_token)
-
-bot=telebot.TeleBot(token=bot_token)
+bot=telebot.TeleBot(token = bot_token)
+server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -53,7 +56,7 @@ def price(message):
             text="#"+str(coin[1].upper()) +"\n*Last Price:* "+"_"+str(response["result"][0]['Last'])+"_ " + str(coin[0]) + "*\nHigh:* _" + str(response["result"][0]['High']) +"_ *\nLow:* _" + str(response["result"][0]['Low']) + "_ *\nVolume:* _" + str(response["result"][0]['Volume']) +"_"
             bot.send_message(chatid,text,parse_mode='Markdown')
         else:
-            print "success:false"
+            print("success:false");
             text="Invalid pair entered,please try again! or use /help"
             bot.send_message(chatid,text,parse_mode='Markdown')
     except Exception:
@@ -61,11 +64,15 @@ def price(message):
         print("your internet is broken :( please try again!")
 
 
-while True:
-    try:
-        bot.polling()
-    except Exception:
-        time.sleep(10)
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://kryptobot-telegram.herokuapp.com/' + bot_token)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 
 
@@ -83,6 +90,19 @@ while True:
 
 
 
+# import telebot
+
+# bot = telebot.TeleBot("477501756:AAFW8vtaZyVe3qF9r-YJqWLFax_ru33-XCM")
+
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+# 	bot.reply_to(message, "Howdy, how are you doing?")
+
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+# 	bot.reply_to(message, message.text)
+
+# bot.polling()
 
 
 
